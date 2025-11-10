8f9a95f56870f045a01a6437176b8d16a6500c7b
BradBKaiBuffs <bkai1@buffs.wtamu.edu>
2025-11-10 16:37:43 -0600

    corrected typo

--- disaster_notification/settings.py
+++ disaster_notification/settings.py
@@ -83,7 +83,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=DATABASE=URL
+        default=DATABASE_URL
         conn_max_age=600,
         conn_health_checks=True,
         ssl_require=True
