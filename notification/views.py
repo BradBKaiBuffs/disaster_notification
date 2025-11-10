4c9eaa7a269acce2837be2d4484041940edc7b22
BradBKaiBuffs <bkai1@buffs.wtamu.edu>
2025-11-10 16:54:20 -0600

    created html files

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def transit(request):
+    return render(request, "homepage.html")
+
+def dashboard(request):
+    return render(request, "dashboard.html")
