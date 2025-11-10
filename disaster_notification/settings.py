d6244384b92ae186b03103c9024e170332416487
BradBKaiBuffs <bkai1@buffs.wtamu.edu>
2025-11-10 17:21:22 -0600

    corrected typo

--- disaster_notification/settings.py
+++ disaster_notification/settings.py
@@ -138,7 +138,8 @@ STATIC_URL = 'static/'
 
 # add static root files
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = 'static/'
+
+STATICFILES_DIRS = BASE_DIR / 'static',
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
