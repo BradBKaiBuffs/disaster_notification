5217cf30bf9522c1f8793e71a36ffe80789f4ae3
BradBKaiBuffs <bkai1@buffs.wtamu.edu>
2025-11-11 18:48:46 -0600

    modify celery.py

--- disaster_notification/celery.py
+++ disaster_notification/celery.py
@@ -1,15 +1,21 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 # Celery configuration
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'disaster_notification.settings')
 
 # set app
 app = Celery('disaster_notification')
+
+app.conf.broker_url = os.getenv('REDIS_URL')
+
+app.conf.result_backend = os.getenv('REDIS_URL')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # load task modules
-app.autodiscover_tasks
+app.autodiscover_tasks()
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
