b79f5dcdf7498af366e9d4c3f8d364dff53e84fe
BradBKaiBuffs <bkai1@buffs.wtamu.edu>
2025-11-11 21:08:36 -0600

    dashboard added

--- notification/views.py
+++ notification/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import noaa_alerts
+from django.utils.timezone import now
 
 # Create your views here.
 def notification(request):
     return render(request, "homepage.html")
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    # Display all active NOAA alerts
+    active_alerts = noaa_alerts.objects.filter(
+        expires__gt=now()
+    ).order_by('-sent')  # newest first
+
+    context = {
+        'alerts': active_alerts
+    }
+    return render(request, 'notification/dashboard.html', context)
